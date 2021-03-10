97344bc4b8836d26246cbf0b229633cf266ea094
Dennis Grabowski <dennisgrabowski@gmx.de>
2021-03-10 14:47:47 +0100

    app: generify and extract entity compare/diff/update algorithm

--- services/business-services/customer-service/src/main/java/edu/hsh/favs/project/escqrs/services/customerservice/service/CustomerService.java
+++ services/business-services/customer-service/src/main/java/edu/hsh/favs/project/escqrs/services/customerservice/service/CustomerService.java
@@ -1,26 +1,17 @@
 package edu.hsh.favs.project.escqrs.services.customerservice.service;
 
-import static org.javers.core.diff.ListCompareAlgorithm.LEVENSHTEIN_DISTANCE;
-
 import edu.hsh.favs.project.escqrs.domains.customers.Customer;
 import edu.hsh.favs.project.escqrs.events.customer.factories.CustomerCreatedEventFactory;
 import edu.hsh.favs.project.escqrs.events.customer.factories.CustomerDeletedEventFactory;
 import edu.hsh.favs.project.escqrs.events.customer.factories.CustomerUpdatedEventFactory;
 import edu.hsh.favs.project.escqrs.services.commons.DualWriteTransactionHelper;
+import edu.hsh.favs.project.escqrs.services.commons.EntityUpdater;
 import edu.hsh.favs.project.escqrs.services.customerservice.repository.CustomerRepository;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Optional;
-import org.javers.core.Javers;
-import org.javers.core.JaversBuilder;
-import org.javers.core.diff.Diff;
-import org.javers.core.diff.changetype.ValueChange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.reactive.TransactionalOperator;
-import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
 import reactor.util.Logger;
 import reactor.util.Loggers;
@@ -34,6 +25,7 @@ public class CustomerService {
   private final CustomerDeletedEventFactory deleteEventFactory;
   private final CustomerUpdatedEventFactory updateEventFactory;
   private final DualWriteTransactionHelper<Customer> dualWriteHelper;
+  private final EntityUpdater<Customer> entityUpdater;
 
   @Autowired
   public CustomerService(
@@ -47,6 +39,7 @@ public class CustomerService {
     this.updateEventFactory = new CustomerUpdatedEventFactory();
     this.dualWriteHelper =
         new DualWriteTransactionHelper<>(template, txOperator, messageBroker, log);
+    this.entityUpdater = new EntityUpdater<>(log);
   }
 
   public Mono<Customer> find(Long id) {
@@ -68,36 +61,12 @@ public class CustomerService {
     // customerId given on the URI path
     // TODO: handle error where user will try to "create" an user via an update
     // ^- both errors are conceptually the same, I think
-    Javers javers = JaversBuilder.javers().withListCompareAlgorithm(LEVENSHTEIN_DISTANCE).build();
     return this.repo
         .findById(customerId)
         .flatMap(
-            customer -> {
-              Diff diff = javers.compare(customer, updatedCustomer);
-              this.log.info(String.format("Logging difference between objects: %s", diff));
-              diff.getChangesByType(ValueChange.class)
-                  .forEach(
-                      valueChange -> {
-                        try {
-                          Optional<?> opt = Optional.ofNullable(valueChange.getRight());
-                          if (opt.isPresent()) {
-                            Method method =
-                                Customer.class.getDeclaredMethod(
-                                    "set" + StringUtils.capitalize(valueChange.getPropertyName()),
-                                    valueChange.getRight().getClass());
-                            method.invoke(customer, valueChange.getRight());
-                          }
-                        } catch (IllegalAccessException e) {
-                          e.printStackTrace();
-                        } catch (InvocationTargetException e) {
-                          e.printStackTrace();
-                        } catch (NoSuchMethodException e) {
-                          e.printStackTrace();
-                        }
-                      });
-              return dualWriteHelper.updateEntity(
-                  customer,
-                  updateEventFactory.supplyEntity(updatedCustomer.setId(customer.getId())));
-            });
+            customer ->
+                dualWriteHelper.updateEntity(
+                    entityUpdater.update(customer, updatedCustomer),
+                    updateEventFactory.supplyEntity(updatedCustomer.setId(customer.getId()))));
   }
 }
---
+++ services/business-services/service-commons/src/main/java/edu/hsh/favs/project/escqrs/services/commons/EntityUpdater.java
@@ -0,0 +1,51 @@
+package edu.hsh.favs.project.escqrs.services.commons;
+
+import static org.javers.core.diff.ListCompareAlgorithm.LEVENSHTEIN_DISTANCE;
+
+import edu.hsh.favs.project.escqrs.domains.customers.Customer;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Optional;
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.javers.core.diff.Diff;
+import org.javers.core.diff.changetype.ValueChange;
+import org.springframework.util.StringUtils;
+import reactor.util.Logger;
+
+public class EntityUpdater<EntityT> {
+
+  private final Logger log;
+  private final Javers javers;
+
+  public EntityUpdater(Logger log) {
+    this.log = log;
+    this.javers = JaversBuilder.javers().withListCompareAlgorithm(LEVENSHTEIN_DISTANCE).build();
+  }
+
+  public EntityT update(EntityT oldEntity, EntityT newEntity) {
+    Diff diff = javers.compare(oldEntity, newEntity);
+    this.log.info(String.format("Logging difference between objects: %s", diff));
+    diff.getChangesByType(ValueChange.class)
+        .forEach(
+            valueChange -> {
+              try {
+                Optional<?> opt = Optional.ofNullable(valueChange.getRight());
+                if (opt.isPresent()) {
+                  Method method =
+                      oldEntity.getClass().getDeclaredMethod(
+                          "set" + StringUtils.capitalize(valueChange.getPropertyName()),
+                          valueChange.getRight().getClass());
+                  method.invoke(oldEntity, valueChange.getRight());
+                }
+              } catch (IllegalAccessException e) {
+                e.printStackTrace();
+              } catch (InvocationTargetException e) {
+                e.printStackTrace();
+              } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+              }
+            });
+    return oldEntity;
+  }
+}
