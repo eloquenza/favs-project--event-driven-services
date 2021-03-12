5f39f8b07906e7d49c40a907499c6882ad2b5fb9
Dennis Grabowski <dennisgrabowski@gmx.de>
2021-03-12 22:53:10 +0100

    app: business-intelligence-service: rename into ProductMarketAnalysis as we are tracking bought and products customers were interested in

--- application/services/business-services/business-intelligence-service/src/main/java/edu/hsh/favs/project/escqrs/services/businessintelligenceservice/analysis/ProductMarketAnalysis.java
+++ application/services/business-services/business-intelligence-service/src/main/java/edu/hsh/favs/project/escqrs/services/businessintelligenceservice/analysis/ProductMarketAnalysis.java
@@ -7,6 +7,7 @@ import edu.hsh.favs.project.escqrs.events.order.OrderUpdatedEvent;
 import edu.hsh.favs.project.escqrs.services.businessintelligenceservice.config.EventSink;
 import edu.hsh.favs.project.escqrs.services.businessintelligenceservice.datatypes.EntityAnalyticWarehouse;
 import edu.hsh.favs.project.escqrs.services.commons.eventprocessing.EntityEventProcessor;
+import java.util.stream.Stream;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.context.annotation.Configuration;
@@ -16,18 +17,27 @@ import reactor.util.Loggers;
 
 @Configuration
 @EnableBinding(EventSink.class)
-public class AmountOfProductsBoughtMarketAnalysis {
+public class ProductMarketAnalysis {
 
   // Save orders and products by a id <-> id mapping,
   // implements a histogram with productId buckets to count
   // which products have been bought how many times.
-  private final EntityAnalyticWarehouse<Long, Long> warehouse;
+  private final EntityAnalyticWarehouse<Long, Long> productsCustomerWereInterestedInWarehouse;
+  private final EntityAnalyticWarehouse<Long, Long> boughtProductsWarehouse;
   private final EntityEventProcessor eventProcessor;
+  private final Stream<String> finalOrderStates =
+      Stream.of(
+          OrderState.PAID.toString(),
+          OrderState.SHIPPED.toString(),
+          OrderState.DELIVERED.toString());
   private final Logger log;
 
-  public AmountOfProductsBoughtMarketAnalysis() {
-    this.warehouse = new EntityAnalyticWarehouse<>(1L, 0L, (l, r) -> l + 1L, (l, r) -> l - 1L);
-    this.log = Loggers.getLogger(AmountOfProductsBoughtMarketAnalysis.class.getName());
+  public ProductMarketAnalysis() {
+    this.productsCustomerWereInterestedInWarehouse =
+        new EntityAnalyticWarehouse<>(1L, 0L, (l, r) -> l + 1L, (l, r) -> l - 1L);
+    this.boughtProductsWarehouse =
+        new EntityAnalyticWarehouse<>(1L, 0L, (l, r) -> l + 1L, (l, r) -> l - 1L);
+    this.log = Loggers.getLogger(ProductMarketAnalysis.class.getName());
     this.eventProcessor = new EntityEventProcessor(this.log);
   }
 
@@ -38,8 +48,9 @@ public class AmountOfProductsBoughtMarketAnalysis {
     this.eventProcessor.handleEvent(
         orderPlacedEvent,
         event -> {
-          this.warehouse.addValueEntry(orderPlacedEvent.getId(), orderPlacedEvent.getProductId());
-          this.logHistogram();
+          this.productsCustomerWereInterestedInWarehouse.addValueEntry(
+              orderPlacedEvent.getId(), orderPlacedEvent.getProductId());
+          this.logHistograms();
         });
   }
 
@@ -51,9 +62,9 @@ public class AmountOfProductsBoughtMarketAnalysis {
         orderUpdatedEvent,
         event -> {
           if (orderUpdatedEvent.getProductId() != null) {
-            this.warehouse.updateValueEntry(
+            this.productsCustomerWereInterestedInWarehouse.updateValueEntry(
                 orderUpdatedEvent.getId(), orderUpdatedEvent.getProductId());
-            this.logHistogram();
+            this.logHistograms();
           } else {
             this.log.info("Skip productId processing - REASON: no productIds were updated.");
           }
@@ -61,12 +72,20 @@ public class AmountOfProductsBoughtMarketAnalysis {
               .getState()
               .toString()
               .contentEquals(OrderState.CANCELLED.toString())) {
-            this.warehouse.removeValueEntry(orderUpdatedEvent.getId());
-            this.logHistogram();
+            this.productsCustomerWereInterestedInWarehouse.removeValueEntry(
+                orderUpdatedEvent.getId());
+            this.logHistograms();
           } else {
             this.log.info(
                 "Skip updateRemoval processing - REASON: state was not changed into "
                     + OrderState.CANCELLED.toString());
+          }
+          if (orderUpdatedEvent.getState().toString().contentEquals(OrderState.PAID.toString())) {
+            Long productId =
+                this.productsCustomerWereInterestedInWarehouse.getMappedValue(
+                    orderUpdatedEvent.getId());
+            this.boughtProductsWarehouse.addValueEntry(orderUpdatedEvent.getId(), productId);
+            this.logHistograms();
           }
         });
   }
@@ -78,18 +97,27 @@ public class AmountOfProductsBoughtMarketAnalysis {
     this.eventProcessor.handleEvent(
         orderDeletedEvent,
         event -> {
-          if (!orderDeletedEvent
-              .getState()
-              .toString()
-              .contentEquals(OrderState.DELIVERED.toString())) {
-            this.log.info("Order was never delivered, i.e. these products were never bought.");
-            this.warehouse.removeValueEntry(orderDeletedEvent.getId());
-            this.logHistogram();
+          if (finalOrderStates.noneMatch(orderDeletedEvent.getState().toString()::contentEquals)) {
+            this.log.info("Order was never finalized, i.e. these products were never bought.");
+            this.productsCustomerWereInterestedInWarehouse.removeValueEntry(
+                orderDeletedEvent.getId());
+            this.logHistograms();
           }
         });
   }
 
-  private void logHistogram() {
-    this.log.info("Displaying the updated 'products bought' histogram: " + warehouse);
+  private void logHistograms() {
+    this.logInterestingProductsHistogram();
+    this.logBoughtProductsHistogram();
+  }
+
+  private void logInterestingProductsHistogram() {
+    this.log.info(
+        "Displaying the updated 'products customers were interested in' histogram: "
+            + productsCustomerWereInterestedInWarehouse);
+  }
+
+  private void logBoughtProductsHistogram() {
+    this.log.info("Displaying the updated 'products bought' histogram: " + boughtProductsWarehouse);
   }
 }
