673840b555c114acaf7896cb352324fedff95820
Dennis Grabowski <dennisgrabowski@gmx.de>
2021-03-13 04:28:21 +0100

    app: order-service: create an customerId entity in order to allow the R2DBC entity mapper to work

--- application/services/business-services/order-service/src/main/java/edu/hsh/favs/project/escqrs/services/orderservice/consumers/CustomerIdSaver.java
+++ application/services/business-services/order-service/src/main/java/edu/hsh/favs/project/escqrs/services/orderservice/consumers/CustomerIdSaver.java
@@ -3,6 +3,7 @@ package edu.hsh.favs.project.escqrs.services.orderservice.consumers;
 import edu.hsh.favs.project.escqrs.events.customer.CustomerCreatedEvent;
 import edu.hsh.favs.project.escqrs.events.customer.CustomerDeletedEvent;
 import edu.hsh.favs.project.escqrs.services.commons.eventprocessing.EntityEventProcessor;
+import edu.hsh.favs.project.escqrs.services.orderservice.dtos.CustomerId;
 import edu.hsh.favs.project.escqrs.services.orderservice.repository.OrderRepository;
 import edu.hsh.favs.project.escqrs.services.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,6 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.r2dbc.core.DatabaseClient;
-import org.springframework.r2dbc.core.DatabaseClient.GenericExecuteSpec;
-import reactor.core.Disposable;
 import reactor.util.Logger;
 import reactor.util.Loggers;
 
@@ -25,9 +23,9 @@ public class CustomerIdSaver {
 
   private final EntityEventProcessor eventProcessor;
   private final Logger log;
-  private final R2dbcEntityTemplate template;
   private final OrderRepository repository;
   private final OrderService service;
+  private final R2dbcEntityTemplate template;
 
   @Autowired
   public CustomerIdSaver(
@@ -44,7 +42,8 @@ public class CustomerIdSaver {
       condition = EntityEventProcessor.MATCHING_CUSTOMERCREATEDEVENT)
   public void receive(@Payload CustomerCreatedEvent createEvent) {
     this.eventProcessor.handleEvent(
-        createEvent, event -> insertCustomerIdFromDB(createEvent.getId()));
+        createEvent,
+        event -> template.insert(new CustomerId(createEvent.getId())).single().then().subscribe());
   }
 
   @StreamListener(
@@ -57,31 +56,15 @@ public class CustomerIdSaver {
           repository
               .findByCustomerId(deletedEvent.getId())
               .flatMap(order -> service.deleteOrder(order.getId()))
-              .doOnComplete(() -> deleteCustomerIdFromDB(deletedEvent.getId()))
+              .doOnComplete(
+                  () ->
+                      template
+                          .delete(new CustomerId(deletedEvent.getId()))
+                          .single()
+                          .then()
+                          .subscribe())
               .then()
               .subscribe();
         });
-  }
-
-  private DatabaseClient getDBClient() {
-    return template.getDatabaseClient();
-  }
-
-  private Disposable insertCustomerIdFromDB(Long customerId) {
-    return executeSqlStatement(
-        getDBClient()
-            .sql("INSERT INTO orderservice_customers (id) VALUES($1)")
-            .bind(0, customerId));
-  }
-
-  private Disposable deleteCustomerIdFromDB(Long customerId) {
-    return executeSqlStatement(
-        getDBClient()
-            .sql("DELETE FROM orderservice_customers c WHERE c.id=$1")
-            .bind(0, customerId));
-  }
-
-  private Disposable executeSqlStatement(GenericExecuteSpec statement) {
-    return statement.fetch().rowsUpdated().single().then().subscribe();
   }
 }
---
+++ application/services/business-services/order-service/src/main/java/edu/hsh/favs/project/escqrs/services/orderservice/dtos/CustomerId.java
@@ -0,0 +1,43 @@
+package edu.hsh.favs.project.escqrs.services.orderservice.dtos;
+
+import java.util.Objects;
+import org.javers.core.metamodel.annotation.ValueObject;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@ValueObject
+@Table(value = "orderservice_customers")
+public class CustomerId {
+
+  @Id private Long id;
+
+  public CustomerId(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof CustomerId)) {
+      return false;
+    }
+    CustomerId that = (CustomerId) o;
+    return getId().equals(that.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId());
+  }
+
+  @Override
+  public String toString() {
+    return "CustomerId{" + "id=" + id + '}';
+  }
+}
