b45b446f3e59930e537a30a9b5a06a82bc487bba
Dennis Grabowski <dennisgrabowski@gmx.de>
2021-03-13 01:30:07 +0100

    app: domains: order: represent in/valid state transitions through the use of enum classes

--- application/domains/orders/src/main/java/edu/hsh/favs/project/escqrs/domains/orders/OrderState.java
+++ application/domains/orders/src/main/java/edu/hsh/favs/project/escqrs/domains/orders/OrderState.java
@@ -3,9 +3,117 @@ package edu.hsh.favs.project.escqrs.domains.orders;
 import java.io.Serializable;
 
 public enum OrderState implements Serializable {
-  PLACED,
-  PAID,
-  SHIPPED,
-  DELIVERED,
-  CANCELLED
+  PLACED {
+    @Override
+    public boolean hasPreviousState() {
+      return false;
+    }
+
+    @Override
+    public OrderState[] previousState() {
+      return new OrderState[] {};
+    }
+
+    @Override
+    public OrderState[] nextState() {
+      return new OrderState[] {PAID, CANCELLED};
+    }
+
+    @Override
+    public boolean hasNextState() {
+      return true;
+    }
+  },
+  PAID {
+    @Override
+    public boolean hasPreviousState() {
+      return true;
+    }
+
+    @Override
+    public OrderState[] previousState() {
+      return new OrderState[] {PLACED};
+    }
+
+    @Override
+    public OrderState[] nextState() {
+      return new OrderState[] {SHIPPED, CANCELLED};
+    }
+
+    @Override
+    public boolean hasNextState() {
+      return true;
+    }
+  },
+  SHIPPED {
+    @Override
+    public boolean hasPreviousState() {
+      return true;
+    }
+
+    @Override
+    public OrderState[] previousState() {
+      return new OrderState[] {PAID};
+    }
+
+    @Override
+    public OrderState[] nextState() {
+      return new OrderState[] {DELIVERED};
+    }
+
+    @Override
+    public boolean hasNextState() {
+      return true;
+    }
+  },
+  DELIVERED {
+    @Override
+    public boolean hasPreviousState() {
+      return true;
+    }
+
+    @Override
+    public OrderState[] previousState() {
+      return new OrderState[] {SHIPPED};
+    }
+
+    @Override
+    public OrderState[] nextState() {
+      return new OrderState[] {};
+    }
+
+    @Override
+    public boolean hasNextState() {
+      return false;
+    }
+  },
+  CANCELLED {
+    @Override
+    public boolean hasPreviousState() {
+      return true;
+    }
+
+    @Override
+    public OrderState[] previousState() {
+      return new OrderState[] {PLACED, PAID};
+    }
+
+    @Override
+    public OrderState[] nextState() {
+      return new OrderState[] {};
+    }
+
+    @Override
+    public boolean hasNextState() {
+      return false;
+    }
+  };
+
+  public abstract boolean hasPreviousState();
+
+  public abstract OrderState[] previousState();
+
+  public abstract boolean hasNextState();
+
+  public abstract OrderState[] nextState();
 }
