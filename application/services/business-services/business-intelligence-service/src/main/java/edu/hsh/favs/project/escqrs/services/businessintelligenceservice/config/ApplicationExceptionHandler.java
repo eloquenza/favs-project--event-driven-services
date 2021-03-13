c87e7527dcf5337b3b18804bf69623d8ee96b91e
Dennis Grabowski <dennisgrabowski@gmx.de>
2021-03-13 03:47:04 +0100

    app: services: register GlobalExceptionHandler as response exception handler to set correct http status code and message

---
+++ application/services/business-services/business-intelligence-service/src/main/java/edu/hsh/favs/project/escqrs/services/businessintelligenceservice/config/ApplicationExceptionHandler.java
@@ -0,0 +1,9 @@
+package edu.hsh.favs.project.escqrs.services.businessintelligenceservice.config;
+
+import edu.hsh.favs.project.escqrs.services.commons.web.errorhandling.GlobalExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler extends GlobalExceptionHandler {
+  // See super class for explanation on why this class is empty.
+}
