88c18f8e87fdf122bd3d2ebcf5cc8dc5ebd29b4b
Dennis Grabowski <dennisgrabowski@gmx.de>
2021-03-14 05:10:04 +0100

    app: product-query-service: add schema registry client

---
+++ application/services/business-services/product-query-service/src/main/java/edu/hsh/favs/project/escqrs/services/productqueryservice/config/SchemaRegistryConfig.java
@@ -0,0 +1,30 @@
+package edu.hsh.favs.project.escqrs.services.productqueryservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cloud.stream.schema.client.ConfluentSchemaRegistryClient;
+import org.springframework.cloud.stream.schema.client.SchemaRegistryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SchemaRegistryConfig {
+
+  @Bean
+  public SchemaRegistryClient schemaRegistryClient(
+      @Value("${spring.cloud.stream.kafka.binder.producer-properties.schema.registry.url}")
+          String endPoint) {
+    ConfluentSchemaRegistryClient client = new ConfluentSchemaRegistryClient();
+    client.setEndpoint(endPoint);
+    return client;
+  }
+
+  // Spring does not seem to be able to autoconfigure a cacheManager itself, so we are providing one
+  @Bean
+  @ConditionalOnMissingBean
+  public CacheManager cacheManager() {
+    return new ConcurrentMapCacheManager();
+  }
+}
